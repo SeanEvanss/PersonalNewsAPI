aae1bd7cddc54b338ee73fb01f15a214f379dca2
Sean Tang <1996sean@gmail.com>
2022-03-15 23:45:00 +0800

    Initial commit with presently required dependencies

---
+++ src/main/java/com/seanevans/customfeed/CustomFeedApplication.java
@@ -0,0 +1,13 @@
+package com.seanevans.customfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomFeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomFeedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seanevans/customfeed/CustomFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seanevans.customfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomFeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
